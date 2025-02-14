dd8ae6016c154b033f3b3d1a0c3a8072e398042f
WarGhost132 <wawghostick@gmail.com>
2025-02-14 15:09:19 +0300

    Global Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@nuxtjs/cloudinary',
+    'nuxt-auth-utils'
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  },
+  runtimeConfig: {
+    githubId: '',
+    githubSecret: '',
+    stripeSecret: '',
+    stripeWebhookSecret: '',
+    public: {
+      cloudinaryCloudName: '',
+      uploadPreset: '',
+      stripeKey: '',
+    },
+    cloudinaryApiKey: '',
+    cloudinaryApiSecret: '',
+  },
+  cloudinary: {
+    cloudName: process.env.NUXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    uploadPreset: process.env.NUXT_PUBLIC_UPLOAD_PRESET,
+    apiKey: process.env.NUXT_CLOUDINARY_API_KEY,
+  },
 })
